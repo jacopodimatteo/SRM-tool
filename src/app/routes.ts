832a59742b267a5dc68a304d0c4697829fcbd2dd
Jacopo Dimatteo <jakodim04@gmail.com>
2026-03-04 19:11:43 +0100

    Fix blank page on GitHub Pages

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,11 +1,11 @@
-import { createBrowserRouter } from "react-router";
+import { createHashRouter } from "react-router";
 import { Layout } from "./components/Layout";
 import { SupplierOverview } from "./pages/SupplierOverview";
 import { SupplierDetail } from "./pages/SupplierDetail";
 import { PortfolioView } from "./pages/PortfolioView";
 import { Settings } from "./pages/Settings";
 
-export const router = createBrowserRouter([
+export const router = createHashRouter([
   {
     path: "/",
     Component: Layout,
